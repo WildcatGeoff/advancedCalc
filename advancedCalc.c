3ff6412ba5b126483a814fb2385b92b3b4106f58
WildcatGeoff <130526370+WildcatGeoff@users.noreply.github.com>
2023-10-10 19:32:50 -0700

    Added init,push,pop,print functions for Stack datatype

--- advancedCalc.c
+++ advancedCalc.c
@@ -19,11 +19,56 @@ struct Queue
 };
 
 //function declartions
-void push(struct Stack *s);
+void initStack(struct Stack *s);
+void push(struct Stack *s,double value);
 double pop(struct Stack *s);
+void printStack(struct Stack *s);
 
 int main()
 {
 	printf("Advanced Calc Starting!\n");
+	struct Stack s;
+	initStack(&s);
+	printStack(&s);
+	push(&s,33);
+	push(&s,123);
+	printStack(&s);
+	printf("Popped %f \n",pop(&s));
+	printStack(&s);
 	return 0;
+}
+
+void initStack(struct Stack *s)
+{
+	s->tos = 0;
+}
+void push(struct Stack *s,double value)
+{
+	s->stack[s->tos] = value;
+	s->tos++;
+}
+double pop(struct Stack *s)
+{
+	s->tos--;
+	double num = s->stack[s->tos];
+	return num;
+}
+void printStack(struct Stack *s)
+{
+	for(int i=s->tos-1;i>=0;i--)
+	{
+		if(i != 0)
+		{
+			printf("%f, ",s->stack[i]);
+		}
+		else
+		{
+			printf("%f\n",s->stack[i]);
+		}
+	}
+	
+	if(s->tos <= 0)
+	{
+		printf("Stack is empty\n");
+	}
 }
