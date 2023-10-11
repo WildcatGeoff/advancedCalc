b2b37cd771edfb6ec83d87c4278958858000345a
WildcatGeoff <130526370+WildcatGeoff@users.noreply.github.com>
2023-10-10 19:06:41 -0700

    Added stack and queue structs

--- advancedCalc.c
+++ advancedCalc.c
@@ -3,6 +3,25 @@
 #include <string.h>
 #include <stdlib.h>
 
+//stack and queue needed for implementing shunting-yard algorithm
+struct Stack
+{
+	double stack[100];
+	int tos;
+};
+
+struct Queue
+{
+	struct Stack s1;
+	struct Stack s2;
+	int foq;
+	int baq;
+};
+
+//function declartions
+void push(struct Stack *s);
+double pop(struct Stack *s);
+
 int main()
 {
 	printf("Advanced Calc Starting!\n");
