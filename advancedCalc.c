1656c82a5d2490504806348ad3dd0c5830137b23
WildcatGeoff <130526370+WildcatGeoff@users.noreply.github.com>
2023-10-11 21:44:58 -0700

    Added way to input expression, created stack for tokenizing numbers

--- advancedCalc.c
+++ advancedCalc.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 //stack and queue needed for implementing shunting-yard algorithm
 struct Stack
@@ -25,6 +26,7 @@ void initStack(struct Stack *s);
 void push(struct Stack *s,double value);
 double pop(struct Stack *s);
 void printStack(struct Stack *s);
+double buildNum(struct Stack *s);
 void initQueue(struct Queue *q);
 void enqueue(struct Queue *q,double value);
 double dequeue(struct Queue *q);
@@ -32,27 +34,53 @@ void printQueue(struct Queue *q);
 
 int main()
 {
-	printf("Advanced Calc Starting!\n");
-	struct Stack s;
-	initStack(&s);
-	printStack(&s);
-	push(&s,33);
-	push(&s,123);
-	printStack(&s);
-	printf("Popped %f \n",pop(&s));
-	printStack(&s);
-	printf("Queue test time \n");
-	struct Queue q;
-	initQueue(&q);
-	printQueue(&q);
-	enqueue(&q,3.14);
-	enqueue(&q,9999);
-	enqueue(&q,1337);
-	printQueue(&q);
-	dequeue(&q);
-	printQueue(&q);
-	dequeue(&q);
-	printQueue(&q);
+	char buffer[4096]; //buffer used for keyboard input
+	bool calcRunning = true;
+	struct Stack operatorStack;
+	initStack(&operatorStack);
+	struct Stack numberStack;//for tokenizing numbers
+	initStack(&numberStack);
+	struct Queue outputQueue;
+	initQueue(&outputQueue);
+	bool buildingANumber = false;
+	double tempNum = 0;
+	while(calcRunning)
+	{
+		printf("Calculator>");
+		fgets(buffer,sizeof(buffer),stdin); //get input from keyboard
+		char c;
+		for(int i=0;i<4096;i++)
+		{
+			c = buffer[i];
+			if(c == 0)
+			{
+				if(buildingANumber)
+				{
+					tempNum = buildNum(&numberStack);
+					printf("Number built: %f\n",tempNum);
+					buildingANumber = false;
+				}
+				break;
+			}
+			//48-57 ascii codes for numbers
+			if(c >= 48 && c <= 57)
+			{
+				tempNum = c - 48;
+				push(&numberStack,tempNum);
+				buildingANumber = true;
+			}
+			else
+			{
+				if(buildingANumber)
+				{
+					tempNum = buildNum(&numberStack);
+					printf("Number built: %f\n",tempNum);
+					buildingANumber = false;
+				}
+			}
+		}
+		calcRunning = false;
+	}
 	return 0;
 }
 
@@ -92,6 +120,30 @@ void printStack(struct Stack *s)
 	{
 		printf("Stack is empty\n");
 	}
+}
+//idea behind this function is that when the main program finds ascii number characters
+//it will push them onto a special stack
+//once all the characters are found, it will convert the characters into a double
+//by popping them off the stack and multiplying by the power of 10 that it is
+double buildNum(struct Stack *s)
+{
+	double value = 0;
+	double temp = 0;
+	int power = 0;
+	if(s->tos > 0)
+	{
+		while(s->tos > 0)
+		{
+			temp = pop(s);
+			value = value + (pow(10,power)*temp);
+			power++;
+		}
+	}
+	else
+	{
+		value = s->stack[0];
+	}
+	return value;
 }
 void initQueue(struct Queue *q)
 {
